a82d35557deba965cee89c95d69ee66a08467397
Michael Stewart <mstewart@uber.com>
2016-12-29 09:02:41 -0800

    Add config files and top level directories

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, '/client/index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: ['./client/index.js'],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true, // this is required to make react router work as expected
+  },
+};

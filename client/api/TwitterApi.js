1c4beeb46de31493d73359ea5bfd5336fd8760bf
istewart <istewart3141@gmail.com>
2016-12-31 14:17:30 -0500

    Started on Twitter api client.

---
+++ client/api/TwitterApi.js
@@ -0,0 +1,18 @@
+// @flow
+import Twitter from 'twitter';
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = client.stream('statuses/filter', {track: 'javascript'});
+stream.on('data', function(event) {
+  console.log(event && event.text);
+});
+ 
+stream.on('error', function(error) {
+  throw error;
+});

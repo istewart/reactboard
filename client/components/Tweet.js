85388ff5c867633737ec2116014cd9f609b3c2f0
istewart <istewart3141@gmail.com>
2017-01-01 23:31:55 -0500

    Made Tweets look a little more Twitter-like.

--- client/components/Tweet.js
+++ client/components/Tweet.js
@@ -3,18 +3,23 @@ import React from 'react';
 
 const Tweet = function TweetKappa(props: Object) {
   return (
-    <div className="well" style={{ backgroundColor: 'white', textAlign: 'left' }}>
-      <div className="tweet-image" style={{ float: 'left', margin: '1rem' }}>
+    <div className="well" style={{ 
+  	  backgroundColor: 'white',
+  	  textAlign: 'left',
+  	  padding: '9px 12px 9px 12px',
+  	  marginBottom: '10px',
+    }}>
+      <div className="tweet-image" style={{ float: 'left', marginRight: '1rem' }}>
         <img
           alt="ok"
-          style={{ width: 50, height: 50 }}
+          style={{ width: '48px', height: '48px', borderRadius: '5px' }}
           src={props.src}
         />
       </div>
-      <div className="tweet-data" style={{ margin: '1rem' }}>
-        <span style={{ fontSize: '14pt', marginRight: '1rem' }}> { props.name } </span>
-        <span style={{ fontSize: '12pt' }}> { props.handle } </span>
-        <p style={{ fontSize: '10pt' }}> { `${props.id} | ${props.text}` } </p>
+      <div className="tweet-data">
+        <span style={{ fontSize: '15px'}}><b> { props.name } </b></span>
+        <span style={{ fontSize: '13px', color: '#8899a6' }}> { props.handle } </span>
+        <p style={{ fontSize: '22px', lineHeight: '28px' }}> { `${props.id} | ${props.text}` } </p>
       </div>
     </div>
   );
